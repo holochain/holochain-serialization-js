8488b367fe9970a1c117165bbe3c037ec2e55425
Jost Schulte <jost.schulte@protonmail.com>
2022-12-20 09:23:18 -0600

    build(rollup): remove import.meta.env

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,10 @@
 import typescript from "@rollup/plugin-typescript";
 import rust from "@wasm-tool/rollup-plugin-rust";
+import replace from "@rollup/plugin-replace";
+
+const match = `    if (typeof input === 'undefined') {
+        input = new URL('index_bg.wasm', import.meta.url);
+    }`;
 
 export default {
   input: "index.ts",
@@ -9,5 +14,9 @@ export default {
       inlineWasm: true,
     }),
     typescript({}),
+    replace({
+      [match]: "",
+      delimiters: ["", ""],
+    }),
   ],
 };
